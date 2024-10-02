50a456b7e9b90e452be11f4b32ada604a82359ea
caiodansk <124446768+caiodansk@users.noreply.github.com>
2024-10-02 20:58:08 -0300

    Revert "feat: adição do navBar na pagina Home"

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterManager } from './routes/RouterManager.jsx';
 import './styles/globalStyles.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterManager />
