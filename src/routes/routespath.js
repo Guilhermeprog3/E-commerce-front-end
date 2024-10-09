96dc57c8601dcb02c5e8f03552895bbdc33f6149
ViniciusF07 <viniciusfrancelino97@gmail.com>
2024-10-08 23:25:37 -0300

    teste

--- src/routes/routespath.js
+++ src/routes/routespath.js
@@ -1,11 +1,13 @@
 import { Home } from '../page/public/home';
 import Checkout from '../page/public/checkout';
 import { pagamentoSim } from '../page/pagamentoOk';
+import { pagamentoRec } from '../page/pagamentoREC';
 
 export const RoutesPath = {
 
   '/': Home,
   '/pagamento_ok': pagamentoSim,
   '/checkout': Checkout,
+  '/pagamento_rec': pagamentoRec,
 
 };
