6502c3e1a9c419a1f3cdba1529673c5bcf75045d
PHziinn <skamikaze4@gmail.com>
2024-11-02 22:45:21 -0300

    🚑 hotfix: Correção na integração de adicionar produtos no back-end

--- src/page/public/adc_Produto/index.jsx
+++ src/page/public/adc_Produto/index.jsx
@@ -1,13 +1,65 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
-import PostProduto from '../../../server/api';
+import { PostProduto } from '../../../server/api';
 
 function Adc_Produto() {
+  const [nome, setNome] = useState('')
+  const [descricao, setDescricao] = useState('')
+  const [preco, setPreco] = useState('')
+  const [estoque, setEstoque] = useState('')
+  const [codigo, setCodigo] = useState('')
+  const [imagem, setImagem] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleImageChange = (e) => {
+    setImagem(e.target.files[0]);
+  };
+
+  const createProduto = async () => {
+    setLoading(true);
+    setError(null);
+
+    const formData = new FormData();
+    formData.append('name', nome);
+    formData.append('description', descricao);
+    formData.append('price', preco);
+    formData.append('stock', estoque);
+    formData.append('sku', codigo);
+
+    if (!imagem) {
+      console.error('Dados inválidos ou faltando.')
+    } else {
+      formData.append('files', imagem);
+    }
+
+
+    try {
+      const dados = await PostProduto(formData)
+
+      setNome('');
+      setDescricao('');
+      setPreco('');
+      setEstoque('');
+      setCodigo('');
+      setImagem(null);
+
+
+    } catch (error) {
+      console.error("Erro ao criar produto:", error);
+      setError("Erro ao criar produto. Verifique os dados e tente novamente.");
+    } finally {
+      setLoading(false)
+    }
+
+  }
+
+
   const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
     clipPath: 'inset(50%)',
@@ -20,33 +72,6 @@ function Adc_Produto() {
     width: 1,
   });
 
-  // Estados para capturar os valores dos campos
-  const [formData, setFormData] = React.useState({
-    nome: '',
-    descricao: '',
-    preco: '',
-    estoque: '',
-    codigo: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [id]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const result = await PostProduto(formData);
-      alert('Produto adicionado com sucesso!', result);
-      setFormData({ nome: '', descricao: '', preco: '', estoque: '', codigo: '' });
-    } catch (error) {
-      alert('Erro ao adicionar produto.');
-    }
-  };
 
   return (
     <div
@@ -61,8 +86,6 @@ function Adc_Produto() {
         elevation={3}
         sx={{ padding: 3, width: '100%', maxWidth: '30ch' }}>
         <Box
-          component="form"
-          onSubmit={handleSubmit}
           sx={{
             display: 'flex',
             flexDirection: 'column',
@@ -74,37 +97,38 @@ function Adc_Produto() {
             id="nome"
             label="Nome"
             variant="outlined"
-            value={formData.nome}
-            onChange={handleInputChange}
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
           />
           <TextField
             id="descricao"
             label="Descrição"
             variant="outlined"
-            value={formData.descricao}
-            onChange={handleInputChange}
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
           />
           <TextField
+            type="number"
             id="preco"
             label="Preço"
             variant="outlined"
-            value={formData.preco}
-            onChange={handleInputChange}
+            value={preco}
+            onChange={(e) => setPreco(e.target.value)}
           />
           <TextField
             type="number"
             id="estoque"
             label="Estoque"
             variant="outlined"
-            value={formData.estoque}
-            onChange={handleInputChange}
+            value={estoque}
+            onChange={(e) => setEstoque(e.target.value)}
           />
           <TextField
             id="codigo"
             label="Código do Produto"
             variant="outlined"
-            value={formData.codigo}
-            onChange={handleInputChange}
+            value={codigo}
+            onChange={(e) => setCodigo(e.target.value)}
           />
 
           <Button
@@ -112,11 +136,12 @@ function Adc_Produto() {
             role={undefined}
             variant="contained"
             sx={{ backgroundColor: 'black', color: 'white' }}
-            startIcon={<CloudUploadIcon />}>
+            startIcon={<CloudUploadIcon />}
+            onChange={handleImageChange}
+          >
             Adicionar Imagem
             <VisuallyHiddenInput
               type="file"
-              onChange={(event) => console.log(event.target.files)}
               multiple
             />
           </Button>
@@ -124,8 +149,9 @@ function Adc_Produto() {
           <Button
             type="submit"
             variant="contained"
+            onClick={createProduto}
             sx={{ backgroundColor: 'black', color: 'white' }}>
-            Adicionar Produto
+            {loading ? "Adicionando..." : "Adicionar Produto"}
           </Button>
         </Box>
       </Paper>
